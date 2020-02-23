188f7b2a04df62c014780124de66d4c957893a9f
CallumIMD <61361128+CallumIMD@users.noreply.github.com>
2020-02-23 14:30:08 +0000

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,162 @@
+/*
+	Strata by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+  var $window = $(window),
+    $body = $("body"),
+    $header = $("#header"),
+    $footer = $("#footer"),
+    $main = $("#main"),
+    settings = {
+      // Parallax background effect?
+      parallax: true,
+
+      // Parallax factor (lower = more intense, higher = less intense).
+      parallaxFactor: 20
+    };
+
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1800px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: [null, "480px"]
+  });
+
+  // Play initial animations on page load.
+  $window.on("load", function() {
+    window.setTimeout(function() {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
+
+  // Touch?
+  if (browser.mobile) {
+    // Turn on touch mode.
+    $body.addClass("is-touch");
+
+    // Height fix (mostly for iOS).
+    window.setTimeout(function() {
+      $window.scrollTop($window.scrollTop() + 1);
+    }, 0);
+  }
+
+  // Footer.
+  breakpoints.on("<=medium", function() {
+    $footer.insertAfter($main);
+  });
+
+  breakpoints.on(">medium", function() {
+    $footer.appendTo($header);
+  });
+
+  // Header.
+
+  // Parallax background.
+
+  // Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
+  if (browser.name == "ie" || browser.mobile) settings.parallax = false;
+
+  if (settings.parallax) {
+    breakpoints.on("<=medium", function() {
+      $window.off("scroll.strata_parallax");
+      $header.css("background-position", "");
+    });
+
+    breakpoints.on(">medium", function() {
+      $header.css("background-position", "left 0px");
+
+      $window.on("scroll.strata_parallax", function() {
+        $header.css(
+          "background-position",
+          "left " +
+            -1 * (parseInt($window.scrollTop()) / settings.parallaxFactor) +
+            "px"
+        );
+      });
+    });
+
+    $window.on("load", function() {
+      $window.triggerHandler("scroll");
+    });
+  }
+
+  // Main Sections: Two.
+
+  // Lightbox gallery.
+  $window.on("load", function() {
+    $("#two").poptrox({
+      caption: function($a) {
+        return $a.next("h3").text();
+      },
+      overlayColor: "#2c2c2c",
+      overlayOpacity: 0.85,
+      popupCloserText: "",
+      popupLoaderText: "",
+      selector: ".work-item a.image",
+      usePopupCaption: true,
+      usePopupDefaultStyling: false,
+      usePopupEasyClose: false,
+      usePopupNav: true,
+      windowMargin: breakpoints.active("<=small") ? 0 : 50
+    });
+  });
+})(jQuery);
+
+function validateForm() {
+  var name = document.getElementById("name").value;
+  if (name == "") {
+    document.querySelector(".status").innerHTML = "Name cannot be empty";
+    return false;
+  }
+  var email = document.getElementById("email").value;
+  if (email == "") {
+    document.querySelector(".status").innerHTML = "Email cannot be empty";
+    return false;
+  } else {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(email)) {
+      document.querySelector(".status").innerHTML = "Email format invalid";
+      return false;
+    }
+  }
+  var subject = document.getElementById("subject").value;
+  if (subject == "") {
+    document.querySelector(".status").innerHTML = "Subject cannot be empty";
+    return false;
+  }
+  var message = document.getElementById("message").value;
+  if (message == "") {
+    document.querySelector(".status").innerHTML = "Message cannot be empty";
+    return false;
+  }
+  document.querySelector(".status").innerHTML = "Sending...";
+  formData = {
+    name: $("input[name=name]").val(),
+    email: $("input[name=email]").val(),
+    subject: $("input[name=subject]").val(),
+    message: $("textarea[name=message]").val()
+  };
+
+  $.ajax({
+    url: "mail.php",
+    type: "POST",
+    data: formData,
+    success: function(data, textStatus, jqXHR) {
+      $(".status").text(data.message);
+      if (data.code)
+        //If mail was sent successfully, reset the form.
+        $("#contact-form")
+          .closest("form")
+          .find("input[type=text], textarea")
+          .val("");
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $(".status").text(jqXHR);
+    }
+  });
+}
